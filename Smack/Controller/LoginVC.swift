f884983d94522a3fa124c2d76cb85f6ecabde117
Anirudh Bandi <anirudhbandi@Anirudhs-MBP.home>
2018-01-13 14:31:22 -0500

    lesson4

---
+++ Smack/Controller/LoginVC.swift
@@ -0,0 +1,24 @@
+//
+//  LoginVC.swift
+//  Smack
+//
+//  Created by Anirudh Bandi on 1/13/18.
+//  Copyright © 2018 Anirudh Bandi. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+   
+    @IBAction func closePressed(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
---
+++ Smack/Utilities/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  Smack
+//
+//  Created by Anirudh Bandi on 1/13/18.
+//  Copyright © 2018 Anirudh Bandi. All rights reserved.
+//
+
+import Foundation
+
+//Segues
+
+let TO_LOGIN = "toLogin"
+
