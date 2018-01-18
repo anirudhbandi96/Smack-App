3fb0f76e5a06c8dea74fe80c89af5dae4d20b68e
Anirudh Bandi <anirudhbandi@Anirudhs-MBP-X.home>
2018-01-18 10:30:37 -0500

    final

--- Smack/Services/MessageService.swift
+++ Smack/Services/MessageService.swift
@@ -16,14 +16,13 @@ class MessageService {
     
     var channels = [Channel]()
     var messages = [Message]()
+    var unreadChannels = [String]()
     var selectedChannel : Channel?
     
     func findAllChannels(completion: @escaping CompletionHandler){
         
         Alamofire.request(URL_GET_CHANNELS, method: .get, parameters: nil, encoding: JSONEncoding.default, headers: BEARER_HEADER).responseJSON { (response) in
             
-//            print(URL_GET_CHANNELS)
-//            print(AuthService.instance.authToken)
             if response.result.error == nil {
                 
                 guard let data = response.data else { return }
